2da48977cc2bfa15999f88f24d583cb39dd3d1d9
Michał Żuk <87339@stud.uz.zgora.pl>
2018-08-08 17:35:40 +0200

    Initial Settings screen

--- app/src/main/java/io/michalzuk/horton/activities/SettingsActivity.java
+++ app/src/main/java/io/michalzuk/horton/activities/SettingsActivity.java
@@ -1,13 +1,28 @@
 package io.michalzuk.horton.activities;
 
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingsActivity extends AppCompatActivity {
+import io.michalzuk.horton.R;
+
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
     }
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
 }
