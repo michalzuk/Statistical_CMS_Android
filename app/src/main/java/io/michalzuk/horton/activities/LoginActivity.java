9b8a0c0b952dbc8e6adaf0d7d102b56370792264
Michał Żuk <87339@stud.uz.zgora.pl>
2018-10-21 14:46:29 +0200

    Login and Sign Up flow rework

--- app/src/main/java/io/michalzuk/horton/activities/LoginActivity.java
+++ app/src/main/java/io/michalzuk/horton/activities/LoginActivity.java
@@ -1,13 +1,12 @@
 package io.michalzuk.horton.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -17,11 +16,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-import java.util.Objects;
-
 import io.michalzuk.horton.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RelativeLayout loginLayout;
     private FirebaseAuth firebaseAuth;
@@ -29,67 +26,127 @@ public class LoginActivity extends AppCompatActivity {
     private EditText editTextLoginEmail, editTextLoginPassword;
 
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//        loginLayout = findViewById(R.id.activity_login);
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        Button loginButton = findViewById(R.id.login_button);
+//        Button registrationButton = findViewById(R.id.registration_button);
+//        loginProgressBar = findViewById(R.id.login_progressbar);
+//        loginProgressBar.setVisibility(View.INVISIBLE);
+//        editTextLoginEmail = findViewById(R.id.login_mail);
+//        editTextLoginPassword = findViewById(R.id.login_password);
+//        firebaseAuth = FirebaseAuth.getInstance();
+//
+//
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String email = editTextLoginEmail.getText().toString().trim();
+//                String password = editTextLoginPassword.getText().toString().trim();
+//
+//                if (TextUtils.isEmpty(email)) {
+//                    Snackbar.make(loginLayout, getString(R.string.enter_email_address), Snackbar.LENGTH_SHORT).show();
+//                    editTextLoginEmail.requestFocus();
+//                    return;
+//                }
+//
+//                if (TextUtils.isEmpty(password)) {
+//                    Snackbar.make(loginLayout, getString(R.string.enter_password), Snackbar.LENGTH_SHORT).show();
+//                    editTextLoginPassword.requestFocus();
+//                    return;
+//                }
+//
+//                loginProgressBar.setVisibility(View.VISIBLE);
+//
+//                firebaseAuth.signInWithEmailAndPassword(email, password)
+//                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                loginProgressBar.setVisibility(View.GONE);
+//                                if (task.isSuccessful()) {
+//                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                                    startActivity(intent);
+//                                    finish();
+//                                } else {
+//                                    Snackbar.make(loginLayout, Objects.requireNonNull(task.getException()).getMessage(), Snackbar.LENGTH_LONG).show();
+//                                }
+//                            }
+//                        });
+//            }
+//        });
+//
+//        registrationButton.setOnClickListener(new View.OnClickListener() {
+//            final Intent registrationIntent = new Intent(LoginActivity.this, SignUpActivity.class);
+//
+//            @Override
+//            public void onClick(View view) {
+//                LoginActivity.this.startActivity(registrationIntent);
+//            }
+//        });
+//    }
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginLayout = findViewById(R.id.activity_login);
-        firebaseAuth = FirebaseAuth.getInstance();
-        Button loginButton = findViewById(R.id.login_button);
-        Button registrationButton = findViewById(R.id.registration_button);
+        findViewById(R.id.registration_button).setOnClickListener(this);
+        findViewById(R.id.login_button).setOnClickListener(this);
+
         loginProgressBar = findViewById(R.id.login_progressbar);
-        loginProgressBar.setVisibility(View.INVISIBLE);
-        editTextLoginEmail = findViewById(R.id.login_mail);
+        editTextLoginEmail = findViewById(R.id.login_email);
         editTextLoginPassword = findViewById(R.id.login_password);
         firebaseAuth = FirebaseAuth.getInstance();
+        loginProgressBar.setVisibility(View.INVISIBLE);
+        editTextLoginPassword.setAutofillHints(View.AUTOFILL_HINT_PASSWORD);
+        editTextLoginEmail.setAutofillHints(View.AUTOFILL_HINT_EMAIL_ADDRESS);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.registration_button:
+                finish();
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = editTextLoginEmail.getText().toString().trim();
-                String password = editTextLoginPassword.getText().toString().trim();
+            case R.id.login_button:
+                loginUser();
+        }
+    }
 
-                if (TextUtils.isEmpty(email)) {
-                    Snackbar.make(loginLayout, getString(R.string.enter_email_address), Snackbar.LENGTH_SHORT).show();
-                    editTextLoginEmail.requestFocus();
-                    return;
-                }
+    private void loginUser() {
 
-                if (TextUtils.isEmpty(password)) {
-                    Snackbar.make(loginLayout, getString(R.string.enter_password), Snackbar.LENGTH_SHORT).show();
-                    editTextLoginPassword.requestFocus();
-                    return;
-                }
-
-                loginProgressBar.setVisibility(View.VISIBLE);
-
-                firebaseAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                loginProgressBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                } else {
-                                    Snackbar.make(loginLayout, Objects.requireNonNull(task.getException()).getMessage(), Snackbar.LENGTH_LONG).show();
-                                }
-                            }
-                        });
-            }
-        });
-
-        registrationButton.setOnClickListener(new View.OnClickListener() {
-            final Intent registrationIntent = new Intent(LoginActivity.this, SignUpActivity.class);
-
-            @Override
-            public void onClick(View view) {
-                LoginActivity.this.startActivity(registrationIntent);
+        String email = editTextLoginEmail.getText().toString().trim();
+        String password = editTextLoginPassword.getText().toString().trim();
+        loginProgressBar.setVisibility(View.VISIBLE);
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        loginProgressBar.setVisibility(View.GONE);
+                        if(task.isSuccessful()) {
+                            finish();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        } else {
+                            Snackbar.make(loginLayout, task.getException().getMessage(), Snackbar.LENGTH_SHORT).show();
+                        }
             }
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        if (firebaseAuth.getCurrentUser() != null){
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
 }
--- app/src/main/java/io/michalzuk/horton/activities/SignUpActivity.java
+++ app/src/main/java/io/michalzuk/horton/activities/SignUpActivity.java
@@ -1,14 +1,13 @@
 package io.michalzuk.horton.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -17,125 +16,203 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import io.michalzuk.horton.R;
-import io.michalzuk.horton.models.User;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
 
+    private RelativeLayout registerLayout;
     private ProgressBar loginProgressBar;
-    private EditText editTextLoginUsername, editTextLoginEmail, editTextLoginPassword;
+    private EditText editTextLoginEmail, editTextLoginPassword;
     private RelativeLayout relativeLayout;
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
-        relativeLayout = findViewById(R.id.sign_up_layout);
-        firebaseAuth = FirebaseAuth.getInstance();
-        Button createAccount = findViewById(R.id.create_new_account);
-        Button backToLogin = findViewById(R.id.back_to_login);
-        loginProgressBar = findViewById(R.id.login_progressbar);
-        loginProgressBar.setVisibility(View.INVISIBLE);
-        editTextLoginEmail = findViewById(R.id.login_mail);
+
+        registerLayout = findViewById(R.id.sign_up_layout);
+        editTextLoginEmail = findViewById(R.id.login_email);
         editTextLoginPassword = findViewById(R.id.login_password);
-        editTextLoginUsername = findViewById(R.id.login_username);
+        loginProgressBar = findViewById(R.id.login_progressbar);
+        firebaseAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.create_new_account).setOnClickListener(this);
+        loginProgressBar.setVisibility(View.INVISIBLE);
 
-        createAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                registerUser();
-            }
-        });
-
-        backToLogin.setOnClickListener(new View.OnClickListener() {
-            Intent backToLoginIntent = new Intent(SignUpActivity.this, LoginActivity.class);
-
-            @Override
-            public void onClick(View view) {
-                SignUpActivity.this.startActivity(backToLoginIntent);
-            }
-        });
-
-
+        editTextLoginPassword.setAutofillHints(View.AUTOFILL_HINT_PASSWORD);
+        editTextLoginEmail.setAutofillHints(View.AUTOFILL_HINT_EMAIL_ADDRESS);
     }
 
-
     @Override
-    protected void onStart() {
-        super.onStart();
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.create_new_account:
+                registerUser();
+                break;
 
-        if (firebaseAuth.getCurrentUser() != null) {
-            Log.w("Firebase", "User already logged in: " + firebaseAuth.getCurrentUser());
+            case R.id.back_to_login:
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
         }
+
     }
 
     private void registerUser() {
-        final String email = editTextLoginEmail.getText().toString().toLowerCase().trim();
-        final String password = editTextLoginPassword.getText().toString().trim();
-        final String username = editTextLoginUsername.getText().toString().toLowerCase();
-
-        if (email.isEmpty()) {
-            editTextLoginEmail.setError(getString(R.string.error_email_required));
-            editTextLoginEmail.requestFocus();
-            return;
-        }
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editTextLoginEmail.setError(getString(R.string.error_email_invalid));
-            return;
-        }
-
-        if (password.isEmpty()) {
-            editTextLoginPassword.setError(getString(R.string.password_required));
-            editTextLoginPassword.requestFocus();
-            return;
-        }
-
-        if (password.length() <= 8) {
-            editTextLoginPassword.setError(getString(R.string.password_length_incorrect));
-            editTextLoginPassword.requestFocus();
-            return;
-        }
-
-        if (username.length() <= 4) {
-            editTextLoginUsername.setError(getString(R.string.username_length_incorrect));
-            editTextLoginUsername.requestFocus();
-            return;
-        }
+        String email = editTextLoginEmail.getText().toString().trim();
+        String password = editTextLoginPassword.getText().toString().trim();
 
         loginProgressBar.setVisibility(View.VISIBLE);
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            User user = new User(username, email);
 
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    loginProgressBar.setVisibility(View.GONE);
-                                    if (task.isSuccessful()) {
-                                        Snackbar.make(relativeLayout, task.getException().getMessage(), Snackbar.LENGTH_LONG).show();
-                                    } else {
-                                        Snackbar.make(relativeLayout, getString(R.string.something_went_wrong), Snackbar.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
-                        } else {
-                            Snackbar.make(relativeLayout, task.getException().getMessage(), Snackbar.LENGTH_LONG).show();
-                            loginProgressBar.setVisibility(View.GONE);
+        if (email.isEmpty()) {
+            editTextLoginEmail.setError("Email is required");
+            editTextLoginEmail.requestFocus();
+            return;
+        } else if (password.isEmpty()) {
+            editTextLoginPassword.setError("Password is required");
+            editTextLoginPassword.requestFocus();
+            return;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextLoginPassword.setError("Please enter a valid email");
+            editTextLoginEmail.requestFocus();
+            return;
+        } else if (password.length() < 6) {
+            editTextLoginPassword.setError("Minimum password length is equal to 6");
+            editTextLoginPassword.requestFocus();
+            return;
+        } else {
+
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            loginProgressBar.setVisibility(View.INVISIBLE);
+                            if (task.isSuccessful()) {
+                                finish();
+                                Snackbar.make(registerLayout, getString(R.string.successfully_registred), Snackbar.LENGTH_SHORT).show();
+                                startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                            } else if (task.isCanceled()) {
+                                Snackbar.make(registerLayout, getString(R.string.registration_canceled), Snackbar.LENGTH_SHORT).show();
+                            } else if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                Snackbar.make(registerLayout, getString(R.string.already_registered), Snackbar.LENGTH_SHORT).show();
+                            } else {
+                                Snackbar.make(registerLayout, getString(R.string.something_went_wrong), Snackbar.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+
+        }
     }
+
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_sing_up);
+//        relativeLayout = findViewById(R.id.sign_up_layout);
+//        firebaseAuth = FirebaseAuth.getInstance();
+//        Button createAccount = findViewById(R.id.create_new_account);
+//        Button backToLogin = findViewById(R.id.back_to_login);
+//        loginProgressBar = findViewById(R.id.login_progressbar);
+//        loginProgressBar.setVisibility(View.INVISIBLE);
+//        editTextLoginEmail = findViewById(R.id.login_mail);
+//        editTextLoginPassword = findViewById(R.id.login_password);
+//        editTextLoginUsername = findViewById(R.id.login_username);
+//
+//        createAccount.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                registerUser();
+//            }
+//        });
+//
+//        backToLogin.setOnClickListener(new View.OnClickListener() {
+//            Intent backToLoginIntent = new Intent(SignUpActivity.this, LoginActivity.class);
+//
+//            @Override
+//            public void onClick(View view) {
+//                SignUpActivity.this.startActivity(backToLoginIntent);
+//            }
+//        });
+//
+//
+//    }
+
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        if (firebaseAuth.getCurrentUser() != null) {
+//            Log.w("Firebase", "User already logged in: " + firebaseAuth.getCurrentUser());
+//        }
+//    }
+
+//    private void registerUser() {
+//        final String email = editTextLoginEmail.getText().toString().toLowerCase().trim();
+//        final String password = editTextLoginPassword.getText().toString().trim();
+//        final String username = editTextLoginUsername.getText().toString().toLowerCase();
+//
+//        if (email.isEmpty()) {
+//            editTextLoginEmail.setError(getString(R.string.error_email_required));
+//            editTextLoginEmail.requestFocus();
+//            return;
+//        }
+//
+//        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//            editTextLoginEmail.setError(getString(R.string.error_email_invalid));
+//            return;
+//        }
+//
+//        if (password.isEmpty()) {
+//            editTextLoginPassword.setError(getString(R.string.password_required));
+//            editTextLoginPassword.requestFocus();
+//            return;
+//        }
+//
+//        if (password.length() <= 8) {
+//            editTextLoginPassword.setError(getString(R.string.password_length_incorrect));
+//            editTextLoginPassword.requestFocus();
+//            return;
+//        }
+//
+//        if (username.length() <= 4) {
+//            editTextLoginUsername.setError(getString(R.string.username_length_incorrect));
+//            editTextLoginUsername.requestFocus();
+//            return;
+//        }
+//
+//        loginProgressBar.setVisibility(View.VISIBLE);
+//        firebaseAuth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            User user = new User(username, email);
+//
+//                            FirebaseDatabase.getInstance().getReference("Users")
+//                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+//                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<Void> task) {
+//                                    loginProgressBar.setVisibility(View.GONE);
+//                                    if (task.isSuccessful()) {
+//                                        Snackbar.make(relativeLayout, task.getException().getMessage(), Snackbar.LENGTH_LONG).show();
+//                                    } else {
+//                                        Snackbar.make(relativeLayout, getString(R.string.something_went_wrong), Snackbar.LENGTH_LONG).show();
+//                                    }
+//                                }
+//                            });
+//                        } else {
+//                            Snackbar.make(relativeLayout, task.getException().getMessage(), Snackbar.LENGTH_LONG).show();
+//                            loginProgressBar.setVisibility(View.GONE);
+//                        }
+//                    }
+//                });
+//    }
 
 
 }
