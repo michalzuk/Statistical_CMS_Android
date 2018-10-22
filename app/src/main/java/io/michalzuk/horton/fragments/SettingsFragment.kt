04aaa2003f7bead204a86b46ced8da5e6c54dc9d
Michał Żuk <87339@stud.uz.zgora.pl>
2018-10-22 19:35:46 +0200

    Implemented addding credentials to Firebase Database

--- app/src/main/java/io/michalzuk/horton/fragments/SettingsFragment.kt
+++ app/src/main/java/io/michalzuk/horton/fragments/SettingsFragment.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import io.michalzuk.horton.R
 import io.michalzuk.horton.activities.LoginActivity
+import io.michalzuk.horton.models.Credentials
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.fragment_settings.*
 
@@ -17,6 +22,8 @@ import kotlinx.android.synthetic.main.fragment_settings.*
 class SettingsFragment : Fragment() {
 
     private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("credentials");
+    private val firebaseUser: FirebaseUser = mAuth.currentUser!!;
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -40,7 +47,17 @@ class SettingsFragment : Fragment() {
     }
 
     private fun saveCredentials() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val domain = credentials_domain.text.toString().trim()
+        val username: String = credentials_username.text.toString().trim()
+        val apiKey: String = credentials_api_key.text.toString().trim()
+
+        if (!TextUtils.isEmpty(domain)) {
+            val id: String = firebaseUser.uid
+            val credentials = Credentials(domain, username, apiKey)
+            databaseReference.child(id).setValue(credentials)
+            Snackbar.make(view!!.findViewById(R.id.fragment_settings), getString(R.string.credentials_added), Snackbar.LENGTH_SHORT).show()
+
+        }
     }
 }
 
--- app/src/main/java/io/michalzuk/horton/models/Credentials.kt
+++ app/src/main/java/io/michalzuk/horton/models/Credentials.kt
@@ -1,2 +1,4 @@
+package io.michalzuk.horton.models
+
 class Credentials constructor(val domain: String, val  username: String,
                               val apiKey: String)
