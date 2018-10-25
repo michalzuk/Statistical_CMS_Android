b33e673c14b07ec39debff9909e5c8f0d041e30f
Michał Żuk <87339@stud.uz.zgora.pl>
2018-10-25 12:02:04 +0200

    Fixed SignUp fields

--- app/src/main/java/io/michalzuk/horton/activities/RegisterActivity.kt
+++ app/src/main/java/io/michalzuk/horton/activities/RegisterActivity.kt
@@ -4,19 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
+import android.widget.RelativeLayout
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import io.michalzuk.horton.R
 import kotlinx.android.synthetic.main.activity_sing_up.*
 
+
 class RegisterActivity : AppCompatActivity() {
 
     private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var mDatabase: DatabaseReference
+    private var registerLayout: RelativeLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,37 +31,48 @@ class RegisterActivity : AppCompatActivity() {
         val createNewAccountButton = findViewById<View>(R.id.create_new_account) as Button
 
         mDatabase = FirebaseDatabase.getInstance().getReference("Names")
+        registerLayout = findViewById(R.id.activity_sign_up)
 
 
         createNewAccountButton.setOnClickListener { view ->
-            register()
+            registerUser()
         }
     }
 
-    private fun register() {
-        val emailText = findViewById<View>(R.id.login_mail) as EditText
-        val passwordText = findViewById<View>(R.id.login_password) as EditText
-        val usernameText = findViewById<View>(R.id.login_username) as EditText
+    private fun registerUser() {
+        val email = login_mail.text.toString().trim()
+        val password = login_password.text.toString().trim()
 
-        val email = emailText.text.toString()
-        val password = passwordText.text.toString()
-        val username = usernameText.text.toString()
+        login_progressbar.setVisibility(View.VISIBLE)
 
-        if (email.isEmpty() || password.isEmpty() || username.isEmpty()) {
-            Snackbar.make(activity_sign_up, "XXX", Snackbar.LENGTH_LONG).show()
+        if (email.isEmpty()) {
+            login_mail.error = "Email is required"
+            login_mail.requestFocus()
+        } else if (password.isEmpty()) {
+            login_password.error = "Password is required"
+            login_password.requestFocus()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            login_password.error = "Please enter a valid email"
+            login_mail.requestFocus()
+        } else if (password.length < 6) {
+            login_password.error = "Minimum password length is equal to 6"
+            login_password.requestFocus()
         } else {
-            mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            val user = mAuth.currentUser
-                            val uid = user!!.uid
-                            mDatabase.child(uid).child("Name").setValue(username)
-                            Snackbar.make(activity_sign_up, "XXX", Snackbar.LENGTH_LONG).show()
-                            startActivity(Intent(this, MainActivity::class.java))
-                        } else {
-                            Snackbar.make(activity_sign_up, "XXX", Snackbar.LENGTH_LONG).show()
-                        }
-                    }
+
+            this.mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task: Task<AuthResult> ->
+                if (task.isSuccessful) {
+                    finish()
+                    Snackbar.make(registerLayout!!, getString(R.string.successfully_registred), Snackbar.LENGTH_SHORT).show()
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else if (task.isCanceled) {
+                    Snackbar.make(registerLayout!!, getString(R.string.registration_canceled), Snackbar.LENGTH_SHORT).show()
+                } else if (task.exception is FirebaseAuthUserCollisionException) {
+                    Snackbar.make(registerLayout!!, getString(R.string.already_registered), Snackbar.LENGTH_SHORT).show()
+                } else {
+                    Snackbar.make(registerLayout!!, getString(R.string.something_went_wrong), Snackbar.LENGTH_SHORT).show()
+                }
+            }
+
         }
     }
 }
