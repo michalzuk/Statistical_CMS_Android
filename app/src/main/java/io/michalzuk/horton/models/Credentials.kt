974de6e26043ef13fe8456b6c0645bae9e2c5e61
Michał Żuk <87339@stud.uz.zgora.pl>
2018-11-08 18:16:39 +0100

    Tiny credentials fix

--- app/src/main/java/io/michalzuk/horton/models/Credentials.kt
+++ app/src/main/java/io/michalzuk/horton/models/Credentials.kt
@@ -1,4 +1,4 @@
 package io.michalzuk.horton.models
 
-data class Credentials (val domain: String? = null, val apiKey: String? = null,
-                        val username: String? = null)
+data class Credentials (val domain: String? = null, val username: String? = null,
+                        val apiKey: String? = null)
